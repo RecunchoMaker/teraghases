07edc936f97fa31ef0213b5a331080db1b0ba086
Rafa Couto <caligari@treboada.net>
2019-03-13 08:55:35 +0100

    Renamed blink to systick.

--- test/systick/src/main.c
+++ test/systick/src/main.c
@@ -7,22 +7,27 @@
 // 72 MHz / 8
 #define SYSTEM_CLOCK (72000000 / 8)
 
-volatile uint32_t msTicks = 0;
+
+volatile uint32_t sysMillis = 0;
 
 
-void SysTick_Handler(void)  {
-      msTicks++;
+void SysTick_Handler(void)
+{
+      sysMillis++;
 }
 
 
 void setup()
 {
+    // clocks setup
     rcc_clock_setup_in_hse_8mhz_out_72mhz();
     rcc_periph_clock_enable(RCC_GPIOB);
 
+    // gpio setup
     gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
             GPIO_CNF_OUTPUT_PUSHPULL, GPIO1);
 
+    // systick every millisecond
     SysTick_Config(SYSTEM_CLOCK / 1000);
 }
 
@@ -32,15 +37,17 @@ int main(void)
     setup();
 
     uint32_t last_second = 0;
-	while (1)
+    while (1)
     {
-        uint32_t seconds_now = msTicks / 1000;
+        uint32_t seconds_now = sysMillis / 1000;
         if (last_second != seconds_now)
         {
             last_second = seconds_now;
-		    gpio_toggle(GPIOB, GPIO1);
+            gpio_toggle(GPIOB, GPIO1);
         }
-	}
+    }
 
-	return 0;
+    return 0;
 }
+
+
