740fae07e7702df2f44277dea793f4401e1d7512
Rafa Couto <caligari@treboada.net>
2019-03-17 17:31:53 +0100

    2 PWM channels in one timer.

--- test/pwm/main.c
+++ test/pwm/main.c
@@ -6,14 +6,17 @@
 
 void setup_pwm()
 {
-    // enable GPIO port A
+    // enable GPIOA clock
     rcc_periph_clock_enable(RCC_GPIOA);
 
-    // enable TIM2 timer
+    // enable TIM2 clock
     rcc_periph_clock_enable(RCC_TIM2);
 
-    // configure PA1 alt function
+    // configure PA1 alt function (TIM2_CH2)
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO1);
+
+    // configure PA2 alt function (TIM2_CH3)
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO2);
 
     // init TIM2
     timer_disable_counter(TIM2);
@@ -29,13 +32,19 @@ void setup_pwm()
     timer_continuous_mode(TIM2);
     timer_set_period(TIM2, 1000);
 
-    // configure output-compare in PWM1 mode
+    // configure output-compare CH2 in PWM1 mode
     timer_disable_oc_output(TIM2, TIM_OC2);
     timer_set_oc_mode(TIM2, TIM_OC2, TIM_OCM_PWM1);
+    timer_set_oc_value(TIM2, TIM_OC2, 0);
     timer_enable_oc_output(TIM2, TIM_OC2);
 
-    // set duty 0% and enable TIM2
-    timer_set_oc_value(TIM2, TIM_OC2, 0);
+    // configure output-compare CH3 in PWM1 mode
+    timer_disable_oc_output(TIM2, TIM_OC3);
+    timer_set_oc_mode(TIM2, TIM_OC3, TIM_OCM_PWM1);
+    timer_set_oc_value(TIM2, TIM_OC3, 0);
+    timer_enable_oc_output(TIM2, TIM_OC3);
+
+    // enable TIM2
     timer_enable_counter(TIM2);
 }
 
@@ -55,21 +64,25 @@ int main(void)
     setup();
 
     // milliseconds and direction
-    uint16_t ms = 0;
+    int16_t ms = 0;
     int dir = +1;
 
     while (1)
     {
         // little pause
-        for (int n = 0; n < 10000; n++);
+        for (int n = 0; n < 20000; n++);
 
         // up and down value
         ms += dir;
         if (ms == 1000) dir = -1;
         if (ms == 0) dir = +1;
 
-        // set the duty to the quadratic function
+        // ms2 is complementary
+        int32_t ms2 = 1000 - ms;
+
+        // duty to the quadratic function on CH2 and CH3 of TIM2
         timer_set_oc_value(TIM2, TIM_OC2, ms * ms / 1000);
+        timer_set_oc_value(TIM2, TIM_OC3, ms2 * ms2 / 1000);
     }
 
     return 0;
